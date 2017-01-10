6f0c3d2e0af3388d481860e38219335e54940b87
shengs <shery.sheng@emc.com>
2017-01-10 09:59:05 +0800

    add index

--- app.py
+++ app.py
@@ -1,26 +1,8 @@
-from flask import Flask
-from flask import request
+from flask import Flask,render_template
 
-app = Flask(__name__)
+@application.route('/')
+def showMachineList():
+    return render_template('main.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return '<h1>Home</h1>'
-
-@app.route('/signin', methods=['GET'])
-def signin_form():
-    return '''<form action="/signin" method="post">
-              <p><input name="username"></p>
-              <p><input name="password" type="password"></p>
-              <p><button type="submit">Sign In</button></p>
-              </form>'''
-
-@app.route('/signin', methods=['POST'])
-def signin():
-
-    if request.form['username']=='admin' and request.form['password']=='password':
-        return '<h3>Hello, admin!</h3>'
-    return '<h3>Bad username or password.</h3>'
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
