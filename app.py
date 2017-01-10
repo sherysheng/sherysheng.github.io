79648ee0dc22abe28c17576ba68e854c86d9c75f
shengs <shery.sheng@emc.com>
2017-01-10 10:41:10 +0800

    rename

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 
 @application.route('/')
 def showMachineList():
-    return render_template('main.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
