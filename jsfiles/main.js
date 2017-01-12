914d63340cc16048c11e7fe507a76e78ef8bcff5
shengs <shery.sheng@emc.com>
2017-01-12 22:55:44 +0800

    add camera

--- jsfiles/main.js
+++ jsfiles/main.js
@@ -3,3 +3,17 @@ app.controller('myCtrl', function($scope) {
     $scope.firstName= "John";
     $scope.lastName= "Doe";
 });
+
+app.config(function($routeProvider) {
+    $routeProvider
+        // route for the home page
+        .when('/foods', {
+            templateUrl : '/templates/foods.html',
+            controller  : 'FoodController'
+        })
+
+})
+
+app.controller("FoodController", function($scope, $routerParams){
+    console.log("welcome to foodie world");//
+})
