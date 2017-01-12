3ea1169952b9e1684df3df65fbfb21faa2d0fed6
shengs <shery.sheng@emc.com>
2017-01-12 17:30:20 +0800

    add foods

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return render_template('index.html')
+    return render_template('foods.html')
 
 if __name__ == "__main__":
     app.run()
